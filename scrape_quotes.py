ee5059c772591698de981639f860a44bdb1dca8e
Sanjana Kumari <sksanjana1502@gmail.com>
2026-06-25 22:50:35 +0530

    Refactor scrape_quotes.py for clarity and structure

--- scrape_quotes.py
+++ scrape_quotes.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 url = "https://quotes.toscrape.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
